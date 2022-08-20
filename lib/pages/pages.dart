3bef36ed1eb5ee389216bd3220ea573eaf1a7502
Baxriddin OFFICIAL <108660627+baxriddin2006@users.noreply.github.com>
2022-08-20 20:47:43 +0500

    changed 20:47

--- lib/pages/pages.dart
+++ lib/pages/pages.dart
@@ -1,9 +1,13 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:flutter/material.dart';
+import 'package:smooth_page_indicator/smooth_page_indicator.dart';
 import 'package:taxiapp/pages/3pages/1_page.dart';
 import 'package:taxiapp/pages/3pages/2_pge.dart';
 import 'package:taxiapp/pages/3pages/3_page.dart';
+
 class Pages extends StatefulWidget {
-  static final String id="pages";
+  static final String id = "pages";
   const Pages({Key? key}) : super(key: key);
 
   @override
@@ -11,19 +15,57 @@ class Pages extends StatefulWidget {
 }
 
 class _PagesState extends State<Pages> {
+  PageController _boshqaruv = PageController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:Stack(
+      body: Stack(
         children: [
           PageView(
-            children: [
+            children: const [
               One_page(),
               Two_page(),
               Three_page(),
             ],
           ),
+          //inicator
+          Container(
+            alignment: Alignment(0, 0.75),
+            child: SmoothPageIndicator(
+              controller: _boshqaruv, // PageController
+              count: 3,
+
+            ),
+          ),
+        Positioned(
+          top: 720,
+          child: Container(
+            width: 393,
+            height: 65,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(34),
+              boxShadow: [
+                BoxShadow(
+                  color: Color(0x3fff7a00),
+                  blurRadius: 10,
+                  offset: Offset(0, 5),
+                ),
+              ],
+              color: Color(0xffffbd00),
+            ),
+            child: Center(child:  Text(
+              "Keyingisi",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 25,
+                fontFamily: "Jost",
+                fontWeight: FontWeight.w700,
+              ),
+            ),),
+          ),),
+
 
         ],
       ),
